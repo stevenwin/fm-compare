6b10869b7efdc228708ba919482936c820744121
stevenwin <stenguye@gmail.com>
2017-01-26 23:01:17 -0800

    new sd

---
+++ sd.js
@@ -0,0 +1,15 @@
+const sherdog = require('sherdog');
+const request = require('request');
+var fighter_url = require('./sherdogurl.js')
+var fighters = []
+
+function findSherdogURL (fighterName) {
+   for (var i=0;i<fighter_url.length;i++) {
+      for (var name in fighter_url[i]) {
+         if (fighter_url[i][name] == fighterName) {
+            return "http://www.sherdog.com"+fighter_url[i].url;
+            //console.log('fighter_url.'+name, '=', fighter_url[i][name]);
+         }
+      }
+   }
+}
