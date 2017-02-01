1305233c3dcb68d1ce10992a76120fd84c269f10
stenguye <stenguye@gmail.com>
2017-02-01 17:30:14 -0500

    async js tests

--- testfunc.js
+++ testfunc.js
@@ -1,74 +1,64 @@
-/*// Date string parser into int(date) in years
-function dateToDay(date) {
-var d = Date.parse(date);
-var minutes = 1000 * 60;
-var minute = d / minutes
-var hour = minute / 60
-var day = hour / 24
-var year = day / 365
-return day;
+const sherdog = require('sherdog');
+const request = require('request');
+const async = require('async');
+var fighter_url = require('./sherdogurl.js')
+var fm_list = require('./fm_list.js')
+
+var fighters = []
+var fights = []
+var url_fm = 'http://liveapi.fightmetric.com/V1/802/Fnt.json';
+var url_fmLinkSource 
+var url_fighterA = "http://www.sherdog.com/fighter/Conor-McGregor-29688";
+var url_fighterB = "";
+var events = []
+
+for (var j=0;j<fm_list.length;j++) {
+async.waterfall([
+   function getEvent(callback) {
+      request({
+      url: fm_list[j].fm_url,
+      json: true
+      }, function getFights(error, response, data) {
+      if (!error && response.statusCode == 200) {
+        callback(null, data);
+      }
+      })
+   },
+   function getFights(data, callback) {
+      for (var i=0;i<data.FMLiveFeed.Fights.length;i++) {
+         var fight = {
+         date: data.FMLiveFeed.Date,
+         fighter1_name: "",
+         fighter1_url: "",
+         fighter1_outcome: "",
+         fighter2_name: "",
+         fighter2_url: ""
+         }
+
+         fight.fighter2_name = data.FMLiveFeed.Fights[i].Fighters[1].FullName
+         fight.fighter2_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[1].FullName)
+         fight.fighter1_name = data.FMLiveFeed.Fights[i].Fighters[0].FullName
+         fight.fighter1_outcome = data.FMLiveFeed.Fights[i].Fighters[0].Outcome
+         fight.fighter1_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[0].FullName)
+         fights.push(fight);
+      	}
+      callback(null, fights);
+   },
+   function getSherdog(fights) {
+		 for (var k=0;k<fights.length;k++) {
+      console.log(fights[k].fighter1_name+""+k)
+   	}
+	 }
+])
 }
 
-// Date string parser into int(date) in days
-function dateToYear(date) {
-var d = Date.parse(date);
-var minutes = 1000 * 60;
-var minute = d / minutes
-var hour = minute / 60
-var day = hour / 24
-var year = day / 365
-return year;
+function findSherdogURL (fighterName) {
+   for (var i=0;i<fighter_url.length;i++) {
+      for (var name in fighter_url[i]) {
+         if (fighter_url[i][name] == fighterName) {
+            return "http://www.sherdog.com"+fighter_url[i].url;
+            //console.log('fighter_url.'+name, '=', fighter_url[i][name]);
+         }
+      }
+   }
 }
-
-date1 = '2016-12-30';
-date2 = 'Jan/09/2013';
-
-console.log(dateToYear(date1)-dateToYear(date2));*/
-
-
-
-
-/*fm_stats = [{
-  "FMLiveFeed": {
-    "EventID": "802",
-    "Timestamp": "2016/12/30 22:09:44",
-    "Date": "2016-12-30",
-    "Time": "19:30:00",
-    "GMT": "2016-12-31T00:30Z",
-    "Venue": "T-Mobile Arena",
-    "Country": "USA",
-    "City": "Las Vegas",
-    "CurFight": "6370",
-    "EventStart": "16:30:07",
-    "EventEnd": "21:56:08",
-    }
-}]
-    
-function addOne(data) {
-	data+" One";
-}    
-
-
-testis = map(fm_stats, function() {
-	addOne
-});
-
-*/
-const async = require('async')
-
-functionStack = []
-
-function firstRun(callback) {
-    callback(null, "From the first function")
-}
-
-function secondRun(callback) {
-    callback(null, "From the second function")
-}
-
-functionStack.push(firstRun)
-functionStack.push(secondRun)
-
-async.series(functionStack, function(err, result) {
-    
-})
