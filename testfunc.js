dd60ff74bc4bfd24dd8c6d97f4aeb8cf51e53bfb
stevenwin <stenguye@gmail.com>
2017-02-01 23:05:40 -0800

    minor

--- testfunc.js
+++ testfunc.js
@@ -13,43 +13,41 @@ var url_fighterB = "";
 var events = []
 
 for (var j=0;j<fm_list.length;j++) {
-async.waterfall([
-   function getEvent(callback) {
-      request({
-      url: fm_list[j].fm_url,
-      json: true
-      }, function getFights(error, response, data) {
-      if (!error && response.statusCode == 200) {
-        callback(null, data);
-      }
-      })
-   },
-   function getFights(data, callback) {
-      for (var i=0;i<data.FMLiveFeed.Fights.length;i++) {
-         var fight = {
-         date: data.FMLiveFeed.Date,
-         fighter1_name: "",
-         fighter1_url: "",
-         fighter1_outcome: "",
-         fighter2_name: "",
-         fighter2_url: ""
+   async.waterfall([
+      function getEvent(callback) {
+         request({
+         url: fm_list[j].fm_url,
+         json: true
+         }, function getFights(error, response, data) {
+         if (!error && response.statusCode == 200) {
+           callback(null, data);
          }
+         })
+      },
+      function getFights(data, callback) {
+         for (var i=0;i<data.FMLiveFeed.Fights.length;i++) {
+            var fight = {
+            date: data.FMLiveFeed.Date,
+            fighter1_name: "",
+            fighter1_url: "",
+            fighter1_outcome: "",
+            fighter2_name: "",
+            fighter2_url: ""
+            }
 
-         fight.fighter2_name = data.FMLiveFeed.Fights[i].Fighters[1].FullName
-         fight.fighter2_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[1].FullName)
-         fight.fighter1_name = data.FMLiveFeed.Fights[i].Fighters[0].FullName
-         fight.fighter1_outcome = data.FMLiveFeed.Fights[i].Fighters[0].Outcome
-         fight.fighter1_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[0].FullName)
-         fights.push(fight);
-      	}
-      callback(null, fights);
-   },
-   function getSherdog(fights) {
-		 for (var k=0;k<fights.length;k++) {
-      console.log(fights[k].fighter1_name+""+k)
-   	}
-	 }
-])
+            fight.fighter2_name = data.FMLiveFeed.Fights[i].Fighters[1].FullName
+            fight.fighter2_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[1].FullName)
+            fight.fighter1_name = data.FMLiveFeed.Fights[i].Fighters[0].FullName
+            fight.fighter1_outcome = data.FMLiveFeed.Fights[i].Fighters[0].Outcome
+            fight.fighter1_url = findSherdogURL(data.FMLiveFeed.Fights[i].Fighters[0].FullName)
+            fights.push(fight);
+         	}
+         callback(null, fights);
+      },
+      function getSherdog(fights) {
+         console.log(fights)
+      }
+   ])
 }
 
 function findSherdogURL (fighterName) {
