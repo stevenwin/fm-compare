d3acd6029f18fe16a3664e1d6b73a72249d2804f
stevenwin <stenguye@gmail.com>
2017-02-11 14:34:20 -0800

    checked names against fm names

--- testfunc.js
+++
@@ -1,9 +0,0 @@
-const sherdog = require('sherdog');
-const request = require('request');
-const async = require('async');
-const colors = require('colors');
-var fighter_url = require('./sherdogurl.js')
-var fm_list = require('./fm_list.js')
-
-var today = new Date()
-
